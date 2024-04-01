d0aa96408bf110acd800b55b008d5359afc9639f
MAHAM42 <161121868+MAHAM42@users.noreply.github.com>
2024-04-01 20:05:52 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk"
+let todos=[];
+
+while(true){
+ let input = await inquirer.prompt([
+        {
+                name:"todoitem",
+                type:"input",
+                message:chalk.bgRedBright.whiteBright`"ADD TODO ITEM IN LISTING?"`
+        },
+        {
+                name:"addMore",
+                type:"list",
+                message:chalk.bgGreen.yellowBright`"DO YOU WANT TO ADD MORE?"`,
+                choices:["yes" , "no"]
+        },
+]);
+
+          const{todoitem,addMore}=input;
+          todos.push(todoitem);
+          if(addMore == "no") {
+            console.log("Todolist");
+        for(let i=0;i<todos.length; i++){
+            console.log(todos[i])}
+
+           break;
+}
+}
